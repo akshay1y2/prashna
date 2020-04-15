bf97b04411573ecd62e21b2f7cf69f8ce1d8f683
akshay1y2 <akshay1y2@gmail.com>
2020-04-15 14:30:28 +0530

    added bootstrap in forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def errors_explanation_div(model)
+    return if model.errors.empty?
+    content_tag(:div, id: "error_explanation") do
+      header = "#{pluralize(model.errors.count, 'error')} prohibited this #{model.class.name} from being saved:"
+      list_items = model.errors.full_messages.map { |msg| content_tag(:li, msg) }
+      content_tag(:h2, header) + content_tag(:ul, list_items.join.html_safe)
+    end
+  end
 end
